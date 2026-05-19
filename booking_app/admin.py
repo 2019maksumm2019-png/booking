2e931d0d4f34b163f8e28ce79eaf4f8b27a768b4
2019maksumm2019-png <2019maksumm2019>
2026-05-19 20:33:52 +0300

    first commit

---
+++ booking_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import Category, Apartments, Booking
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Apartments)
+admin.site.register(Booking)
