bcab0770fc5aa1c9e0b6d7ee093cfa686967ac86
2019maksumm2019-png <2019maksumm2019>
2026-05-26 20:29:58 +0300

    додали edd  home page

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Apartments, Category, Booking
+
 
 # Create your views here.
+def home(request):
+    apartments = Apartments.objects.all()
+    return render(request, 'index.html',{'name': 'Booking App', 'apartments': apartments})
