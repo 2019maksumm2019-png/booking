da1cca76546f675dbd144f41dc4bd101ab5afb67
2019maksumm2019-png <2019maksumm2019>
2026-06-09 20:30:11 +0300

    коміт

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home_page'),
-    path('apartment/<int:apartment_id>/', views.booking_page, name='booking_page'),
+    path('apartment/<int:apartment_id>/', views.booking_page, name="booking_page"),
+    path('confirmation/<int:booking_id>/', views.booking_confirmation, name="booking_confirmation"),
 ]
--- booking_app/views.py
+++ booking_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Apartments, Category, Booking
 
 
@@ -10,6 +10,30 @@ def home(request):
 def booking_page(request, apartment_id):
 
     apartment = Apartments.objects.get(id=apartment_id)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        check_in = request.POST.get('check_in')
+        check_out = request.POST.get('check_out')
+
+        booking = Booking.objects.create(
+            apartment=apartment,
+            name=name,
+            email=email,
+            phone=phone,
+            check_in=check_in,
+            check_out=check_out
+        )
+
+
+        return redirect('booking_confirmation', booking_id=booking.id)
+
+
     context = {"apart": apartment}
-    
     return render(request, 'booking.html', context)
+
+def booking_confirmation(request, booking_id):
+    booking = Booking.objects.get(id=booking_id)
+    apartment = booking.apartment
+    return render(request, 'booking_comfirmed.html', {'booking': booking, 'apart': apartment})
